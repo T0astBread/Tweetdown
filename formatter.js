6fc9a0f00f21c5f22011e85047ebc244d664390d
T0ast <m.briedl@live.at>
2018-04-08 00:49:00 +0200

    Now recursively parsing DOM tree instead of modifying innerHTML
    Now the formatter recursively traverses the DOM tree under a formattable element instead of modifying that element's innerHTML directly. This prevents attributes from being interpreted as Markdown and formatted.
    
    This resolves #2.

--- formatter.js
+++ formatter.js
@@ -1,3 +1,4 @@
+//QUERY SELECTORS
 const formattableQuerySelectors = {
     webClient: {
         tweets: ".tweet-text",
@@ -19,12 +20,57 @@ const formattableQuerySelectorString =
         return Object.keys(client).map(selector => client[selector])
     }).join(", ")
 
+
 const astInCodeExp = /([^`]*`[^`]*)\*([^`]*`[^`]*)/g //To escape asterisks in code tags
 const underscInCodeExp = /([^`]*`[^`]*)_([^`]*`[^`]*)/g //To escape underscores in code tags
 
 let doingChanges = false
 
 const createCodeElement = (content, isBlock) => `<span class='tweetdwn-code-wrapper ${isBlock ? "block" : ""}'><span class='tweetdwn-placeholder'>// Code</span><code>${content}</code></span>`
+
+
+//RECURSIVE DOM PARSING
+const formatLeafElement = element => {
+    let html = element.innerHTML
+
+    //Escaping special characters in code tags
+    while(astInCodeExp.test(html)) html = html.replace(astInCodeExp, "$1~~~esc~ast~~~$2")
+    while(underscInCodeExp.test(html)) html = html.replace(underscInCodeExp, "$1~~~esc~und~~~$2")
+    //I know that bit was copied but eh
+    //Escaping escaped thingies
+    html = html.replace(/\\`/g, "~~~esc~this~thing~~~")
+
+    //CODE
+    //Code blocks
+    html = html.replace(/```\s*([^`]+)\s*```/g, createCodeElement("$1", true))
+    //Inline code
+    html = html.replace(/`([^`\n]+)`/g, createCodeElement("$1", false))
+    //Implicit code blocks
+    html = html.replace(/`\s*([^`]+)\s*`/g, createCodeElement("$1", true))
+    //BOLD
+    html = html.replace(/((?:\*\*)|(?:__))(.+?)\1/g, "<strong>$2</strong>")
+    //ITALIC
+    html = html.replace(/(\*|_)(.+?)\1/g, "<i>$2</i>")
+
+    //Substitute the escape-placeholders
+    html = html.replace(/~~~esc~ast~~~/g, "*")
+    html = html.replace(/~~~esc~und~~~/g, "_")
+    html = html.replace(/~~~esc~this~thing~~~/g, "`")
+
+    element.innerHTML = html
+}
+
+const formatBranchElement = element => {
+    for(let j = 0; j < element.children.length; j++) {
+        formatElement(element.children[j])
+    }
+}
+
+const formatElement = element => {
+    if(element.children.length > 0) formatBranchElement(element)
+    else formatLeafElement(element)
+}
+
 
 const format = () => {
     if(doingChanges) return
@@ -36,33 +82,8 @@ const format = () => {
         const element = elements[i]
         if(element.hasAttribute("tweetdown-formatted")) continue
 
-        let html = element.innerHTML
+        formatElement(element)
         
-        //Escaping special characters in code tags
-        while(astInCodeExp.test(html)) html = html.replace(astInCodeExp, "$1~~~esc~ast~~~$2")
-        while(underscInCodeExp.test(html)) html = html.replace(underscInCodeExp, "$1~~~esc~und~~~$2")
-        //I know that bit was copied but eh
-        //Escaping escaped thingies
-        html = html.replace(/\\`/g, "~~~esc~this~thing~~~")
-
-        //CODE
-        //Code blocks
-        html = html.replace(/```\s*([^`]+)\s*```/g, createCodeElement("$1", true))
-        //Inline code
-        html = html.replace(/`([^`\n]+)`/g, createCodeElement("$1", false))
-        //Implicit code blocks
-        html = html.replace(/`\s*([^`]+)\s*`/g, createCodeElement("$1", true))
-        //BOLD
-        html = html.replace(/((?:\*\*)|(?:__))(.+?)\1/g, "<strong>$2</strong>")
-        //ITALIC
-        html = html.replace(/(\*|_)(.+?)\1/g, "<i>$2</i>")
-
-        //Substitute the escape-placeholders
-        html = html.replace(/~~~esc~ast~~~/g, "*")
-        html = html.replace(/~~~esc~und~~~/g, "_")
-        html = html.replace(/~~~esc~this~thing~~~/g, "`")
-
-        element.innerHTML = html
         element.setAttribute("tweetdown-formatted", "true")
     }
     setTimeout(() => doingChanges = false, 200)
